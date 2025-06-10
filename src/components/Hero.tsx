6f32671b89656b6ef126b2250e50f7af501db9a6
justanuragmaurya <justanuragmaurya@gmail.com>
2025-06-10 10:33:08 +0530

    Hero section completed

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,9 +1,9 @@
 import Image from "next/image";
-import { Button } from "./ui/button";
 import { File, Mail } from "lucide-react";
 import Link from "next/link";
 import { Bricolage_Grotesque } from "next/font/google";
 import { RainbowButton } from "@/components/magicui/rainbow-button";
+import { AuroraText } from "@/components/magicui/aurora-text";
 
 const font = Bricolage_Grotesque({
     subsets:["latin"],
@@ -24,11 +24,11 @@ export default function HeroSection(){
             </div>
             <div className="text-center md:text-left px-4 md:px-0 max-w-2xl">
                 <h1 className={`text-2xl md:text-3xl font-medium`}>Hey there,</h1>
-                <h1 className={`text-4xl md:text-5xl font-semibold`}>I'm <span className={`bg-gradient-to-r from-red-400 to-red-500 bg-clip-text text-transparent ${font.className} font-black`}>Anurag</span></h1>
+                <h1 className={`text-4xl md:text-5xl font-semibold`}>I'm <span className={`${font.className} font-black`}><AuroraText colors={["red","orange"]}>Anurag</AuroraText></span></h1>
                 <p className="text-primary/70 py-3 text-md">
                     I'm a MVP builder and full-stack developer, turning ideas into production-ready products—handling everything from concept to deployment with a complete modern tech stack.
                 </p>
-                <div className="flex flex-wrap gap-3 mt-2 justify-center md:justify-start">
+                <div className="flex flex-wrap gap-3 mt-2 justify-center md:justify-start items-center">
                     <Link href={"/"} target="_blank">
                         <RainbowButton><Mail className="mr-2 h-4 w-4" /> Get in touch</RainbowButton>
                     </Link>
@@ -37,7 +37,6 @@ export default function HeroSection(){
                     </Link>
                 </div>
             </div>
-            {/* Desktop profile image - only shown on md screens and up */}
             <div className="hidden w-full md:block">
                 <Image 
                     src={"https://pbs.twimg.com/profile_images/1928635890999394304/09YwaCu8_400x400.jpg"} 
